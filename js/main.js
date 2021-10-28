4220911f81968b9658b0d76ae4ed08fa6d28d532
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-10-28 20:39:22 +0300

    add nav

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// const listItem = document.querySelector('.list');
+
+// listItem.addEventListener('focusin', handleFocus);
+// // listItem.addEventListener('focusout', handleFocusOut);
+// listItem.addEventListener('mouseover', handleFocus);
+
+// function handleFocus(event) {
+//     if (event.target.querySelector('.products')) {
+//         const target = event.target.querySelector('.products');
+//         if (target.classList.contains('visually-hidden')) {
+//             target.classList.remove('visually-hidden');
+//         }
+//         target.addEventListener('mouseout', handleFocusOut);
+//         return
+//     }
+//     return
+// };
+
+// function handleFocusOut(event) {
+//     const target = event.currentTarget;
+//     console.log(target)
+//     target.classList.add('visually-hidden');
+//     return
+// }
+
+// // if (!target.classList.contains('visually-hidden')) {
+// //         target.classList.add('visually-hidden');
+// //     }
