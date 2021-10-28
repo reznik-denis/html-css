458562d215614cba8b74dd9dd4525d078d598e57
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-10-28 23:24:34 +0300

    tast finish

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
-// const listItem = document.querySelector('.list');
+const hidden = document.querySelector('.hero-bacgorund_description');
+const img = document.querySelector('.img');
+img.addEventListener('click', handleOnClick);
 
-// listItem.addEventListener('focusin', handleFocus);
-// // listItem.addEventListener('focusout', handleFocusOut);
-// listItem.addEventListener('mouseover', handleFocus);
-
-// function handleFocus(event) {
-//     if (event.target.querySelector('.products')) {
-//         const target = event.target.querySelector('.products');
-//         if (target.classList.contains('visually-hidden')) {
-//             target.classList.remove('visually-hidden');
-//         }
-//         target.addEventListener('mouseout', handleFocusOut);
-//         return
-//     }
-//     return
-// };
-
-// function handleFocusOut(event) {
-//     const target = event.currentTarget;
-//     console.log(target)
-//     target.classList.add('visually-hidden');
-//     return
-// }
-
-// // if (!target.classList.contains('visually-hidden')) {
-// //         target.classList.add('visually-hidden');
-// //     }
+function handleOnClick() {
+    hidden.classList.toggle('hidden');
+    return
+}
